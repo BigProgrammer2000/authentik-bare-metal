30f98f9eff69a49d6ce9e7fd2ff7cd5e9c86e84e
Baschtel <s-e.s@t-online.de>
2024-06-23 12:29:16 +0200

    Update 03_clone.sh
    - change python version to 3.12
    - add --without-hashes to poetry export

--- 03_clone.sh
+++ 03_clone.sh
@@ -4,12 +4,12 @@ git clone https://github.com/goauthentik/authentik.git src
 
 cd /opt/authentik/src/
 
-python3.11 -m virtualenv ./.venv
+python3.12 -m virtualenv ./.venv
 source .venv/bin/activate
 
 # Install dependencies
 pip install --no-cache-dir poetry
-poetry export -f requirements.txt --output requirements.txt
-poetry export -f requirements.txt --dev --output requirements-dev.txt
+poetry export -f requirements.txt --without-hashes --output requirements.txt
+poetry export -f requirements.txt --without-hashes --dev --output requirements-dev.txt
 
 pip install --no-cache-dir -r requirements.txt -r requirements-dev.txt
