b967c92603148602f67c849a30d9f21168779bd7
Baschtel <s-e.s@t-online.de>
2024-06-23 12:25:00 +0200

    Update 01_install_deps.sh
    Update Python, Nodejs and Go versions

--- 01_install_deps.sh
+++ 01_install_deps.sh
@@ -1,26 +1,28 @@
 STARTING_DIR=$(pwd)
 
-sudo apt update && sudo apt upgrade -yqq # Ensure up-to-date system
-sudo apt install -yqq curl wget git build-essential libncursesw5-dev libssl-dev \
-     libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev pkg-config libffi-dev zlib1g-dev libxmlsec1 libxmlsec1-dev libxmlsec1-openssl libmaxminddb0 # Install build dependencies
+apt update && apt upgrade -yqq # Ensure up-to-date system
+apt install -yqq curl wget git build-essential libncursesw5-dev libssl-dev \
+     libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev pkg-config libffi-dev \
+     zlib1g-dev libxmlsec1 libxmlsec1-dev libxmlsec1-openssl libmaxminddb0 postgresql-server-dev-15 # Install build dependencies
 
 wget https://github.com/mikefarah/yq/releases/download/v4.30.8/yq_linux_amd64 -O /usr/bin/yq
 chmod +x /usr/bin/yq
 
-wget https://www.python.org/ftp/python/3.11.1/Python-3.11.1.tgz
-tar xzf Python-3.11.1.tgz 
-cd Python-3.11.1
+wget https://www.python.org/ftp/python/3.12.4/Python-3.12.4.tgz
+tar xzf Python-3.12.4.tgz 
+cd Python-3.12.4
 ./configure --enable-optimizations
-sudo make altinstall # Install Python 3.11.1
+make -j $(nproc)
+make altinstall # Install Python 3.12.4
 
 cd $STARTING_DIR
-rm -rf Python-3.11.1.tgz Python-3.11.1
+rm -rf Python-3.12.4.tgz Python-3.12.4
 
-# Install NodeJS 18
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+# Install NodeJS 20
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-# Install Go 1.19
-wget https://golang.org/dl/go1.19.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
-rm -rf go1.19.linux-amd64.tar.gz
+# Install Go 1.22.4
+wget https://golang.org/dl/go1.22.4.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.22.4.linux-amd64.tar.gz
+rm -rf go1.22.4.linux-amd64.tar.gz
